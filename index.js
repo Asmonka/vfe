69c7f60200c3cdea05302d0b8427871ea16ec6d2
Asmonka <46006367+Asmonka@users.noreply.github.com>
2019-11-10 00:43:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const token = "NTI1OTcwODUyNTg0NjIwMDQ2.Dv-YcA.awpix-ITU9cmIosBxlxvNp_t6Gs";
 
 client.on ("ready", () => {
         console.log("ready!");
-        client.user.setAuthor('Hentai', { type: 'WATCHING' });
+        client.user.setActivity('Hentai', { type: 'WATCHING' });
        
 });
 const prefix = "!"
