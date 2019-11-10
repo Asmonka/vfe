84e3ddbebd2f852be6317a0b7da72a2b8ff614af
Asmonka <46006367+Asmonka@users.noreply.github.com>
2019-11-10 14:21:24 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const token = "NTI1OTcwODUyNTg0NjIwMDQ2.Dv-YcA.awpix-ITU9cmIosBxlxvNp_t6Gs";
 client.on ("ready", () => {
         console.log("ready!");
 
-        client.user.setActivity("Hentai", {type: 'WATCHING'});
+        client.user.setActivity("Hentai (!help)", {type: 'WATCHING'});
 });
 
 const prefix = "!";
@@ -26,72 +26,72 @@ client.on ("message", (message) => {
             message.channel.send({files: ["./white/" + imageNumber + ".jpg"] })
         }
 
-        if(msg.startsWith (prefix + "yuri")) {
+        if(msg === (prefix + "yuri")) {
                 message.delete()
                 number = 10;
                 imageNumber = Math.floor(Math.random() * (number - 1 + 1)) + 1;
                 message.channel.send({files: ["./yuri/" + imageNumber + ".jpg"] })
             }
 
-        if(msg.startsWith (prefix + "fortnite")) {
+        if(msg === (prefix + "fortnite")) {
                 message.delete()
                 number = 10;
                 imageNumber = Math.floor(Math.random() * (number - 1 + 1)) + 1;
                 message.channel.send({files: ["./fortnite/" + imageNumber + ".jpg"] })
         }
-        if(msg.startsWith (prefix + "overwatch")) {
+        if(msg === (prefix + "overwatch")) {
                 message.delete();
                 number = 30;
                 imageNumber = Math.floor(Math.random() * (number - 1 + 1)) + 1;
                 message.channel.send({files: ["./overwatch/" + imageNumber + ".jpg"] })
         }
-        if(msg.startsWith (prefix + "loli")) {
+        if(msg === (prefix + "loli")) {
                 message.delete()
                 number = 10;
                 imageNumber = Math.floor(Math.random() * (number - 1 + 1)) + 1;
                 message.channel.send({files: ["./loli/" + imageNumber + ".jpg"] })
         }
-        if(msg.startsWith (prefix + "animal")) {
+        if(msg === (prefix + "animal")) {
                 message.delete();
                 number = 20;
                 imageNumber = Math.floor(Math.random() * (number - 1 + 1)) + 1;
                 message.channel.send({files: ["./animal/" + imageNumber + ".jpg"] })
         }
-        if(msg.startsWith (prefix + "redhead")) {
+        if(msg === (prefix + "redhead")) {
                 message.delete()
                 number = 10;
                 imageNumber = Math.floor(Math.random() * (number - 1 + 1)) + 1;
                 message.channel.send({files: ["./redhead/" + imageNumber + ".jpg"] })
         }
-        if(msg.startsWith (prefix + "yaoi")) {
+        if(msg === (prefix + "yaoi")) {
                 message.delete();
                 number = 10;
                 imageNumber = Math.floor(Math.random() * (number - 1 + 1)) + 1;
                 message.channel.send({files: ["./yaoi/" + imageNumber + ".jpg"] })
         }
-        if(msg.startsWith (prefix + "blonde")) {
+        if(msg === (prefix + "blonde")) {
                 message.delete()
                 number = 10;
                 imageNumber = Math.floor(Math.random() * (number - 1 + 1)) + 1;
                 message.channel.send({files: ["./blonde/" + imageNumber + ".jpg"] })
         }       
-        if(msg.startsWith (prefix + "ayaya")) {
+        if(msg === (prefix + "ayaya")) {
                 message.delete()
                 message.channel.send ({files: ["./ayaya/ayaya.gif"]})
         }
-        if(msg.startsWith (prefix + "tina")) {
+        if(msg === (prefix + "tina")) {
                 message.delete()
                 message.channel.send ({files: ["./tina/tina.gif"]})
         }
-        if(msg.startsWith (prefix + "nani")) {
+        if(msg === (prefix + "nani")) {
                 message.delete()
                 message.channel.send ({files: ["./nani/nani.gif"]})
         }
          
-        if(msg.startsWith (prefix + "command")) {
+        if(msg === (prefix + "help")) {
                 message.delete()
            embed = new discord.RichEmbed ()
-           .setAuthor ("Command")
+           .setAuthor ("Commands")
            .addField ("!animal", "animal picture")
            .addField ("!ayaya", "ayaya GIF") 
            .addField ("!blonde", "blonde picture")
