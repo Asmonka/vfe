a07c5e7997e75361a0321d34222a06eb544a4916
Asmonka <46006367+Asmonka@users.noreply.github.com>
2019-11-10 13:34:17 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const discord = require ('discord.js');
+
 var client = new discord.Client();
-const fs = require('fs');
-const db = require('quick.db');
 
 const token = "NTI1OTcwODUyNTg0NjIwMDQ2.Dv-YcA.awpix-ITU9cmIosBxlxvNp_t6Gs";
 
@@ -14,30 +13,11 @@ client.on ("ready", () => {
 const prefix = "!";
 client.on ("message", (message) => {
 
-        msg = message.content.toUpperCase();
-        sender = message.author;
-        args = message.content.slice(prefix.length).trim().split(" ");
-        cmd = args.shift().toLowerCase
-        
+        message.content.toLowerCase();
 
         if (message.author.bot) return;
 
-        
-        db.updateValue(message.author.id + message.guild.id, 1).then(i => { // You pass it the key, which is authorID + guildID, then pass it an increase which is 1 in this instance.
-                // It also returns the new updated object, which is what we will use.
-        
-                let messages; // Create an empty variable - These IF statements will run if the new amount of messages sent is the same as the number.
-                if (i.value == 25) messages = 25; // Level 1
-                else if (i.value == 50) messages = 50; // Level 2
-                else if (i.value == 100) messages = 100; // Level 3 - You can set these to any number, and any amount of them.
-        
-                if (!isNaN(messages)) { // If messages IS STILL empty, run this.
-                    db.updateValue(`userLevel_${message.author.id + message.guild.id}`, 1).then(o => { // This returns the updated object of userLevel_ID. 
-                        message.channel.send(`You sent ${messages} messages, so you leveled up! You are now level ${o.value}`) // Send their updated level to the channel.
-                    })
-                }
-        
-            })
+        msg = message.content.toLowerCase();
 
         if(msg === (prefix + "white")) {
             message.delete()
